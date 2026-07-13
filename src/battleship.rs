30241bdcbf705b066b615dccd35c05e1722b9193
Bridget Hartin Pasco <isaacpasco@icloud.com>
2026-07-12 19:25:12 -0700

    tmp

--- src/battleship.rs
+++ src/battleship.rs
@@ -1,16 +1,9 @@
+pub use crate::utils::{Fx64, Vec3};
 use std::sync::Arc;
-
-#[derive(Clone, Debug, Copy)]
-pub struct Vector {
-    pub x: i64,
-    pub y: i64,
-    pub z: i64,
-}
-
 #[derive(Debug, Clone, Copy)]
 pub struct EntityVisibleData {
-    pub position: Vector,
-    pub velocity: Vector,
+    pub position: Vec3,
+    pub velocity: Vec3,
     pub health: i64,
 }
 
@@ -19,6 +12,7 @@ pub enum EntityKind {
     Inanimate,
     Ship,
     Missile,
+    Laser,
 }
 
 #[derive(Clone, Copy, Debug)]
@@ -30,9 +24,17 @@ pub enum EntityAllegiance {
 
 #[derive(Clone, Debug)]
 pub struct Entity {
+    pub is_valid: bool,
+    pub generation: u64,
     pub kind: EntityKind,
     pub allegiance: EntityAllegiance,
     pub name: Arc<str>,
     pub current_state: EntityVisibleData,
     pub previous_states: [EntityVisibleData; 256],
+}
+
+pub const MAX_ENTITY_COUNT: usize = 65536;
+#[derive(Clone, Debug)]
+pub struct GameState {
+    pub entities: Box<[Entity]>,
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,23 +1,24 @@
-pub const FIXED_DIVISOR: i64 = 10000;
+pub type FixedType = i128;
+pub const FIXED_DIVISOR: i128 = 10000;
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Fixed {
-    inner: i64,
+    inner: i128,
 }
 
 impl Fixed {
     pub const fn new(v: i64) -> Self {
         Self {
-            inner: v * FIXED_DIVISOR,
+            inner: v as FixedType * FIXED_DIVISOR,
         }
     }
 
     pub const fn new_f64(v: f64) -> Self {
         Self {
-            inner: (v * FIXED_DIVISOR as f64).floor() as i64,
+            inner: (v * FIXED_DIVISOR as f64).floor() as i128,
         }
     }
     pub const fn get_i64(&self) -> i64 {
-        self.inner / FIXED_DIVISOR
+        (self.inner / FIXED_DIVISOR) as i64
     }
 
     pub const fn get_f64(&self) -> f64 {
@@ -47,6 +48,10 @@ impl Fixed {
             inner: (self.inner / rhs.inner),
         }
     }
+
+    pub fn sqrt(&self) -> Self {
+        Self::new_f64(self.get_f64().sqrt())
+    }
 }
 
 impl From<i64> for Fixed {
@@ -60,15 +65,15 @@ impl From<f64> for Fixed {
         Self::new_f64(value)
     }
 }
-impl Into<i64> for Fixed {
-    fn into(self) -> i64 {
-        self.get_i64()
+impl From<Fixed> for i64 {
+    fn from(v: Fixed) -> Self {
+        v.get_i64()
     }
 }
 
-impl Into<f64> for Fixed {
-    fn into(self) -> f64 {
-        self.get_f64()
+impl From<Fixed> for f64 {
+    fn from(v: Fixed) -> Self {
+        v.get_f64()
     }
 }
 
@@ -129,3 +134,133 @@ impl std::fmt::Debug for Fixed {
         write!(f, "{}", self.get_f64())
     }
 }
+
+#[derive(Clone, Debug, Copy, PartialEq, Eq, Hash)]
+pub struct Vector {
+    pub x: Fixed,
+    pub y: Fixed,
+    pub z: Fixed,
+}
+impl Vector {
+    pub const fn new(x: Fixed, y: Fixed, z: Fixed) -> Self {
+        Self { x, y, z }
+    }
+
+    pub const fn new_i64(x: i64, y: i64, z: i64) -> Self {
+        Self {
+            x: Fixed::new(x),
+            y: Fixed::new(y),
+            z: Fixed::new(z),
+        }
+    }
+
+    pub const fn new_f64(x: f64, y: f64, z: f64) -> Self {
+        Self {
+            x: Fixed::new_f64(x),
+            y: Fixed::new_f64(y),
+            z: Fixed::new_f64(z),
+        }
+    }
+
+    pub const fn op_add(&self, rhs: &Self) -> Self {
+        Self {
+            x: self.x.op_add(&rhs.x),
+            y: self.y.op_add(&rhs.y),
+            z: self.z.op_add(&rhs.z),
+        }
+    }
+
+    pub const fn op_sub(&self, rhs: &Self) -> Self {
+        Self {
+            x: self.x.op_sub(&rhs.x),
+            y: self.y.op_sub(&rhs.y),
+            z: self.z.op_sub(&rhs.z),
+        }
+    }
+
+    pub const fn dot(&self, rhs: &Self) -> Fixed {
+        let a = self.x.op_mul(&rhs.x);
+        let b = self.y.op_mul(&rhs.y);
+        let c = self.z.op_mul(&rhs.z);
+        a.op_add(&b.op_add(&c))
+    }
+
+    pub const fn cross(&self, rhs: &Self) -> Self {
+        let x = (self.y.op_mul(&rhs.z)).op_sub(&self.z.op_mul(&rhs.y));
+        let y = (self.z.op_mul(&rhs.x)).op_sub(&self.x.op_mul(&rhs.z));
+        let z = (self.x.op_mul(&rhs.y)).op_sub(&self.y.op_mul(&rhs.x));
+        Self::new(x, y, z)
+    }
+
+    pub const fn scaled(&self, rhs: &Fixed) -> Self {
+        Self::new(self.x.op_mul(rhs), self.y.op_mul(rhs), self.z.op_mul(rhs))
+    }
+    pub const fn divided(&self, rhs: &Fixed) -> Self {
+        Self::new(self.x.op_div(rhs), self.y.op_div(rhs), self.z.op_div(rhs))
+    }
+
+    pub fn len(&self) -> Fixed {
+        (self.x * self.x + self.y * self.y + self.z * self.z).sqrt()
+    }
+
+    pub fn normalized(&self) -> Self {
+        self.scaled(&(Fixed::new(1) / self.len()))
+    }
+}
+
+impl std::ops::Add for Vector {
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self {
+        self.op_add(&rhs)
+    }
+}
+impl std::ops::Sub for Vector {
+    type Output = Self;
+    fn sub(self, rhs: Self) -> Self {
+        self.op_sub(&rhs)
+    }
+}
+
+impl std::ops::AddAssign for Vector {
+    fn add_assign(&mut self, other: Self) {
+        *self = *self + other;
+    }
+}
+
+impl std::ops::SubAssign for Vector {
+    fn sub_assign(&mut self, other: Self) {
+        *self = *self - other;
+    }
+}
+
+impl std::ops::Mul<Fixed> for Vector {
+    type Output = Self;
+    fn mul(self, rhs: Fixed) -> Self {
+        self.scaled(&rhs)
+    }
+}
+
+impl std::ops::Div<Fixed> for Vector {
+    type Output = Self;
+    fn div(self, rhs: Fixed) -> Self {
+        let x = self.x / rhs;
+        let y = self.y / rhs;
+        let z = self.z / rhs;
+        Self::new(x, y, z)
+    }
+}
+
+impl std::ops::MulAssign<Fixed> for Vector {
+    fn mul_assign(&mut self, rhs: Fixed) {
+        *self = *self * rhs;
+    }
+}
+
+impl std::ops::DivAssign<Fixed> for Vector {
+    fn div_assign(&mut self, rhs: Fixed) {
+        *self = *self / rhs;
+    }
+}
+
+pub type Fx64 = Fixed;
+pub type Vec3 = Vector;
